a1c452468cfcc7e64d2485319be53726a7272147
rangex-mitchell <mitchellkim94@icloud.com>
2024-01-06 21:15:36 +0900

    config modified

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'mitchell', // Usually your GitHub org/user name.
+  organizationName: 'rangex-mitchell', // Usually your GitHub org/user name.
   projectName: 'mitchell-dictionary', // Usually your repo name.
 
   onBrokenLinks: 'throw',
