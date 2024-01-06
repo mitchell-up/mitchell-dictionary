1af13606c7ff3bd36f48e9227d6769f9e6c0cb8a
rangex-mitchell <mitchellkim94@icloud.com>
2024-01-06 16:37:23 +0900

    project setup

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,69 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: '현재 공사중입니다',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        웹 프론트엔드 기술기반을 위한 사전을 구축합니다. 간단한 용어의 정리부터 공식문서의 번역 및 요약, 실무적용 사례까지 다양한 글로 채워질 예정입니다.
+      </>
+    ),
+  },
+  // {
+  //   title: 'Focus on What Matters',
+  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+  //       ahead and move your docs into the <code>docs</code> directory.
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Powered by React',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Extend or customize your website layout by reusing React. Docusaurus can
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container text--center">
+        <div className="row" style={{ justifyContent: 'center' }}>
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
