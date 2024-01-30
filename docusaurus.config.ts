be15f65a6e5fcd85d971d1d72b0cbe1bdb2a65d7
rangex-mitchell <mitchellkim94@icloud.com>
2024-01-30 20:56:33 +0900

    feat: SEO 설정 - 구글애널리틱스

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,6 +46,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-QTCKXBPZ9B',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
